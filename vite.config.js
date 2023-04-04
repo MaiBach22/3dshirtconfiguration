46dc2e4368351267557899acf571e7cccc55c380
Emma Pham <emma.pham2017@gmail.com>
2023-04-04 14:11:19 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/3dshirtconfiguration/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
